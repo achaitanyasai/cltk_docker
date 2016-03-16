69e871ac657433a4cebdd473be307cb2a24b6707
chaitanyasai.alaprthi <achaitanyasai@gmail.com>
2016-03-17 00:23:58 +0530

    added tests for latin

--- install.py
+++ install.py
@@ -22,7 +22,6 @@ def _install(lang, lst):
         if _corpus['location'] == 'remote':
             print("    Downloading %s " % (_corpus['name']))
             corpus_importer.import_corpus(_corpus['name'])
-            break
 
 def main():
     _install('latin', LATIN_CORPORA)
--- tests/test_basics.py
+++ tests/test_basics.py
@@ -4,9 +4,10 @@
 __author__ = 'Chaitanya Sai Alaparthi <achaitanyasai@gmail.com>'
 __license__ = 'MIT License. See LICENSE.'
 
+from cltk.corpus.latin.corpora import LATIN_CORPORA
 import sys
 import unittest
-
+import os
 
 class TestSequenceFunctions(unittest.TestCase):  # pylint: disable=R0904
     """Class for unittest"""
@@ -15,6 +16,18 @@ class TestSequenceFunctions(unittest.TestCase):  # pylint: disable=R0904
         """Tests python version"""
         self.assertEqual(sys.version_info.major, 3)
         self.assertEqual(sys.version_info.minor, 5)
+
+    def test_installed_latin_corpora(self):
+        """Tests if latin corpora is correctly installed or not"""
+        path = os.path.expanduser('~/cltk_data/latin/')
+        dirs = []
+        for root, subFolders, files in os.walk(path):
+            for _subFolders in subFolders:
+                dirs.append(_subFolders)
+        for _corpus in LATIN_CORPORA:
+            if _corpus['location'] == 'remote':
+                self.assertTrue(_corpus['name'] in dirs)
+
 
 if __name__ == '__main__':
     unittest.main()
