9b03ac62582fa417489c57081eb041809c5ed97e
chaitanyasai.alaprthi <achaitanyasai@gmail.com>
2016-03-20 13:16:32 +0530

    Added badge to README and corrected run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 sudo docker run -d -it cltk bash
 container_id=`sudo docker ps -a | awk 'FNR == 2 {print $1}'`
 sudo docker cp tests/ $container_id:/
-sudo docker exec $container_id nosetests
+_command='nosetests --no-skip'
+sudo docker exec $container_id $_command
